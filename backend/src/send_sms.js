53f7b6a1760226d3f39cd4fae2cefa1fa93f34b8
Kyle Anderson <kmanderson12@gmail.com>
2019-08-19 12:49:06 -0500

    Update send_sms.js

--- backend/src/send_sms.js
+++ backend/src/send_sms.js
@@ -1,8 +1,8 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
-const accountSid = 'AC2e3173b88e5271c068eafd17f5b4412b';
-const authToken = '3e0b2c6e6a3c1f8e8b1aa08fbd469100';
+const accountSid = 'TWILIO_SID';
+const authToken = 'TWILIO_TOKEN';
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
